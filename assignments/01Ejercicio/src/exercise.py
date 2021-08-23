bce85cdf18a2c376bb3dc853f59e38fcf089e5ca
MaxSando74 <89033313+MaxSando74@users.noreply.github.com>
2021-08-23 11:30:06 -0600

    Update exercise.py

--- assignments/01Ejercicio/src/exercise.py
+++ assignments/01Ejercicio/src/exercise.py
@@ -1,6 +1,19 @@
 def main():
     # Escribe tu código abajo de esta línea
-    pass
+    horas= int(input('Introduce las horas: '))
+    horasseg = horas*3600
+    minutos = int(input('Introduce los minutos: '))
+    minutosseg= minutos*60
+    segundos = int(input('Introduce los segundos: '))
+    proceso1= horasseg+minutosseg+segundos
+    horas2= int(input('Introduce las horas: '))
+    horasseg2 = horas2*3600
+    minutos2 = int(input('Introduce los minutos: '))
+    minutosseg2= minutos2*60
+    segundos2 = int(input('Introduce los segundos: '))
+    proceso2= horasseg2+minutosseg2+segundos2
+    print('proceso 1: ')
+    print('proceso 2: ')
 
 if __name__ == '__main__':
     main()
